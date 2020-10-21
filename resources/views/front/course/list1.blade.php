2f55aa0473d5e706cc9d8b5a1403d77f244bd8fa
kapiljangid1996 <kapiljangid199276@gmail.com>
2020-10-21 19:05:36 +0530

    Update

--- resources/views/front/course/list1.blade.php
+++ resources/views/front/course/list1.blade.php
@@ -5,42 +5,43 @@ Courses
 @stop
 
 @section('content')
-<section class="probootstrap-section probootstrap-section-colored probootstrap-bg probootstrap-custom-heading probootstrap-tab-section" style="background-image: url({{ URL::asset('frontDesign/img/1.jpg') }})">	
+<section class="probootstrap-section probootstrap-section-colored probootstrap-bg probootstrap-custom-heading probootstrap-tab-section" style="background-image: url({{ URL::asset('frontDesign/img/1.jpg') }})">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 text-center section-heading probootstrap-animate">
-				<h2 class="mb0">Our Courses</h2>
+				<h2 class="mb0">Highlights</h2>
 			</div>
 		</div>
 	</div>
 	<div class="probootstrap-tab-style-1">
 		<ul class="nav nav-tabs probootstrap-center probootstrap-tabs no-border">
-			<li class="active"><a data-toggle="tab" href="#all-course">All Course</a></li>
+			<li class="active"><a data-toggle="tab" href="#all-courses">All Courses</a></li>
 			@foreach ($categories as $category)
 				<li><a data-toggle="tab" href="#course-{{ $category->cat_slug }}">{{ $category->cat_name }}</a></li>
 			@endforeach
 		</ul>
+	</div>
 </section>
 <section class="probootstrap-section probootstrap-section">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="tab-content">
-					<div id="all-course" class="tab-pane fade in active">
+					<div id="all-courses" class="tab-pane fade in active">
 						<div class="row">
 							<div class="col-md-12">
 								<div class="owl-carousel" id="owl1">
-								@foreach($courses as $course)
-								<div class="item">						
-									<a href="#" class="probootstrap-featured-news-box"><figure class="probootstrap-media"><img src="/Uploads/Course/{{$course->course_image}}" class="img-responsive"></figure>
-										<div class="probootstrap-text">
-											<h3>{{$course->course_name}}</h3>
-											<p>{{$course->course_short_description}}</p>
-											<span class="probootstrap-date"><i class="icon-calendar"></i>{{ $course->created_at->format('M d, Y') }}</span>
-										</div>
-									</a>
-								</div>
-								@endforeach
+									@foreach($courses as $course)
+									<div class="item">
+										<a href="#" class="probootstrap-featured-news-box"><figure class="probootstrap-media"><img src="/Uploads/Course/{{$course->course_image}}" class="img-responsive"></figure>
+											<div class="probootstrap-text">
+												<h3>{{$course->course_name}}</h3>
+												<p>{{$course->course_short_description}}</p>
+												<span class="probootstrap-date"><i class="icon-calendar"></i>{{ $course->created_at->format('M d, Y') }}</span>
+											</div>
+										</a>
+									</div>
+									@endforeach
 								</div>
 							</div>
 						</div>
@@ -49,10 +50,10 @@ Courses
 					<div id="course-{{ $category->cat_slug }}" class="tab-pane fade">
 						<div class="row">
 							<div class="col-md-12">
-								@foreach($courses as $course)
-								@if($category->cat_name == $course->category->parent->cat_name)
-									<div class="col-md-4">
-									<div class="item">						
+								<div class="owl-carousel" id="owl1-{{ $category->cat_slug }}">
+									@foreach($courses as $course)
+									@if($category->cat_name == $course->category->parent->cat_name)
+									<div class="item">
 										<a href="#" class="probootstrap-featured-news-box"><figure class="probootstrap-media"><img src="/Uploads/Course/{{$course->course_image}}" class="img-responsive"></figure>
 											<div class="probootstrap-text">
 												<h3>{{$course->course_name}}</h3>
@@ -60,9 +61,10 @@ Courses
 												<span class="probootstrap-date"><i class="icon-calendar"></i>{{ $course->created_at->format('M d, Y') }}</span>
 											</div>
 										</a>
-									</div></div>
-								@endif
-								@endforeach
+									</div>
+									@endif
+									@endforeach
+								</div>
 							</div>
 						</div>
 					</div>
@@ -72,7 +74,4 @@ Courses
 		</div>
 	</div>
 </section>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @stop
